alanxz/rabbitmq-c

commit c5cf965ed6d0233468fdea69e8a314f6d46b52c8

lib: correct amqp_time_from_now behavior at {0, 0}
Change behavior of amqp_time_from_now to set to the current timestamp
when a struct timeval of {0, 0} is passed in instead of 0. This better
aligns with how one would expect the function to work.

As a byproduct this corrects an issue where amqp_consume_message would
not return AMQP_STATUS_HEARTBEAT_TIMEOUT when a tv of {0, 0} and the
heartbeat interval had passed.

Fixes #557 in a more concise way.

librabbitmq/amqp_socket.c
@@ -760,7 +760,10 @@ int amqp_try_recv(amqp_connection_state_t state) {
       state->last_queued_frame = link;
     }
   }
-  timeout = amqp_time_immediate();
+  int res = amqp_time_s_from_now(&timeout, 0);
+  if (AMQP_STATUS_OK != res) {
+    return res;
+  }
 
   return recv_with_timeout(state, timeout);
 }

librabbitmq/amqp_time.c
@@ -111,10 +111,6 @@ int amqp_time_from_now(amqp_time_t *time, const struct timeval *timeout) {
     *time = amqp_time_infinite();
     return AMQP_STATUS_OK;
   }
-  if (0 == timeout->tv_sec && 0 == timeout->tv_usec) {
-    *time = amqp_time_immediate();
-    return AMQP_STATUS_OK;
-  }
 
   if (timeout->tv_sec < 0 || timeout->tv_usec < 0) {
     return AMQP_STATUS_INVALID_PARAMETER;
@@ -158,12 +154,6 @@ int amqp_time_s_from_now(amqp_time_t *time, int seconds) {
   }
 
   return AMQP_STATUS_OK;
-}
-
-amqp_time_t amqp_time_immediate(void) {
-  amqp_time_t time;
-  time.time_point_ns = 0;
-  return time;
 }
 
 amqp_time_t amqp_time_infinite(void) {

librabbitmq/amqp_time.h
@@ -62,7 +62,6 @@ uint64_t amqp_get_monotonic_timestamp(void);
 
 /* Get a amqp_time_t that is timeout from now.
  * If timeout is NULL, an amqp_time_infinite() is created.
- * If timeout = {0, 0}, an amqp_time_immediate() is created.
  *
  * Returns AMQP_STATUS_OK on success.
  * AMQP_STATUS_INVALID_PARAMETER if timeout is invalid
@@ -79,9 +78,6 @@ int amqp_time_from_now(amqp_time_t *time, const struct timeval *timeout);
  * fails.
  */
 int amqp_time_s_from_now(amqp_time_t *time, int seconds);
-
-/* Create an immediate amqp_time_t */
-amqp_time_t amqp_time_immediate(void);
 
 /* Create an infinite amqp_time_t */
 amqp_time_t amqp_time_infinite(void);

commit 311205d329d375f79e311371057ea08979adfecb

fix issue with amqp_basic_publish blocking
c5cf965 (#614) introduced a bug where amqp_time_s_from_now(&timeout, 0)
replaced amqp_time_immediate; but specified an infinite timeout instead
of an immediate timeout. This restores this original behavior.

Fixes: #780

Signed-off-by: GitHub <noreply@github.com>

librabbitmq/amqp_socket.c
@@ -729,7 +729,7 @@ int amqp_try_recv(amqp_connection_state_t state) {
       state->last_queued_frame = link;
     }
   }
-  res = amqp_time_s_from_now(&timeout, 0);
+  res = amqp_time_from_now(&timeout, &(struct timeval){0});
   if (AMQP_STATUS_OK != res) {
     return res;
   }